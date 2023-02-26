ed4e33146e53d6313d5b3230add05719fe9055bf
Matthieu-ui <mattfelker206@outlook.com>
2023-02-26 10:08:01 -0800

    mobile *fix

--- CB/src/components/Nav.jsx
+++ CB/src/components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
         <nav className="bg-gray-800 px-4 py-2 rounded-sm shadow-md">
         <div className="flex items-center justify-between">
           <a href="#" className="text-white hover:text-gray-300 font-mono font-bold text-xl">Open Source Community Board</a>
-          <div className="flex items-center">
+          <div className="flex flex-wrap items-center">
             <a href="#" className="text-gray-300 hover:text-white px-3 py-2 text-sm font-medium">Home</a>
             <a href="#" className="text-gray-300 hover:text-white px-3 py-2 text-sm font-medium">About</a>
             <a href="#" className="text-gray-300 hover:text-white px-3 py-2 text-sm font-medium">Contact</a>
